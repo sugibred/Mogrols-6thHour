a18e13b6553dd0cd13def9236266d23835eb1439
Gregory Reese <gr126108@student.wpsok.org>
2026-04-22 14:09:45 -0500

    new testing area

--- Master.py
+++ Master.py
@@ -1,41 +1,4 @@
 #Scrum Document: https://docs.google.com/document/d/1mCz1kTVHPCIYNKEsr3tRNkpy2MMk_BBCFa3kTtJkTCw/edit?tab=t.0
-import random
-vr1=1
-vr2=2
-vr3=3
-vr4=4
-'''item={
-    "vr1":1,
-    "vr2":2,
-    "vr3":3,
-    "vr4":4,
-    "vr5":5,
-    "vr6":6,
-    "vr7":7,
-    "vr8":8,
-    "vr9":9,
-    "vr10":10,
-    "vr11":11,
-    "vr12":12,
-    "vr13":13,
-    "vr14":14,
-    "vr15":15,
-    "vr16":16,
-    "vr17":17,
-    "vr18":18,
-    "vr19":19,
-    "vr20":20,
-}'''
-for i in range(5):
-    print(random.choice(item))
-
-
-
-
-
-
-'''if items["vr20"]+items["vr20"]==40:
-    print("you have won")'''
 
 
 
