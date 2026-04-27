4aad0eceb2ccdd36bb8dbeb52f6f22e1b8b5f8b8
Ethan Nguyen <en128114@student.wpsok.org>
2026-04-27 13:26:58 -0500

    fixed the checking system

--- Semester_Project_Ethan.py
+++ Semester_Project_Ethan.py
@@ -1,4 +1,6 @@
 
+import random
+
 class Name:
     def __init__(self, name, age):
         self.name = name
@@ -12,35 +14,75 @@ class Name:
         self.age = int(input("Enter your age: "))
         if self.age <= 14:
             print("What are you doing here????")
-        else:
+        elif self.age > 14:
             print(f"{self.age} years old")
+        else:
+            print("Error")
 
 
 enter_name = Name("0", 0)
 enter_name.username()
 enter_name.years()
 
-wood = 1
-stone = 2
+item={
+    "headphones":1,
+    "ice bucket":2,
+    "mouse": 3,
+    "microwave": 4,
+    "sponge": 5,
+    "fan": 6,
+    "toothpaste": 7,
+    "remote control": 8,
+    "toaster": 9,
+    "magnet" : 10,
+    "plastic fork": 11,
+    "baby bottle": 12,
+    "teddy bear": 13,
+    "frying pan": 14,
+    "car key": 15,
+    "egg": 16,
+    "battery": 17,
+    "plushie": 18,
+    "cookie": 19,
+    "toilet": 20
+}
 
+starting_itm = random.sample(list(item.keys()), 5)
 
-
-
-def Ask():
+def check():
     ask = input("Want to check an item? (y/n) : ")
-    if ask == "y":
 
-        x = int(input(f"Which item do you want to check? ( 1, 2) : "))
+
+    if ask == "y" or ask == "Y":
+        print(starting_itm)
+
+
+        x = int(input(f"Which item do you want to check? (1, 2, 3, 4, 5) : "))
         if x == 1:
-            print(f"wood is {wood} value")
+            print(f"{starting_itm[0]} has {item[starting_itm[0]]} value")
         elif x == 2:
-            print(f"stone is {stone} value")
+            print(f"{starting_itm[1]} has {item[starting_itm[1]]} value")
+        elif x == 3:
+            print(f"{starting_itm[2]} has {item[starting_itm[2]]} value")
+        elif x == 4:
+            print(f"{starting_itm[3]} has {item[starting_itm[3]]} value")
+        elif x == 5:
+            print(f"{starting_itm[4]} has {item[starting_itm[4]]} value")
 
-    elif ask == "n":
+        check()
+
+
+
+
+    elif ask == "n" or ask == "N":
         print("ok")
+
 
     else:
         print("Error incorrect input")
-        Ask()
+        check()
 
-Ask()
+
+
+
+check()
