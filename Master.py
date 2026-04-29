c7d495c796e4d62d8f021f3d87a4ea791faaae0c
Gregory Reese <gr126108@student.wpsok.org>
2026-04-29 14:02:59 -0500

    new def func

--- Master.py
+++ Master.py
@@ -10,42 +10,56 @@ if user.age <= 5:
     print("want a popsicle")
 starting_val=200
 points = 0
-while points <= starting_val:
-    starting_itm = random.sample(list(item.keys()), 5)
+def game():
+    global points
+    while points <= starting_val:
+        starting_itm = random.sample(list(item.keys()), 5)
 
-    if points >= starting_val:
-        print("you won")
-        break
-    while True:
-
-        def combine():
-            try:
-                global points
-                print(starting_itm)
-                ask1 = input("choose first combo ")
-                ask2=input("choose second combo ")
-                val1 = item.get(ask1)
-                val2 = item.get(ask2)
-                points += val1 + val2
-            except:
-                print("you chose the wrong thing retry")
-                combine()
-        combine()
-        print(points)
-        global ask1
-        global ask2
-        if ask1 in starting_itm:
-            starting_itm.remove(ask1)
-
-
-        if ask2 in starting_itm:
-            starting_itm.remove(ask2)
-
-        starting_itm.append(random.choice(list(item)))
-        starting_itm.append(random.choice(list(item)))
         if points >= starting_val:
-            break
 
+            break
+        while True:
+
+            def combine():
+                try:
+                    global points
+                    global ask1
+                    global ask2
+
+
+                    print(starting_itm)
+                    ask1 = input("choose first combo ")
+                    ask2 = input("choose second combo ")
+                    val1 = item.get(ask1)
+                    val2 = item.get(ask2)
+                    points += val1 + val2
+
+                except:
+                    print("you chose the wrong thing retry")
+                    combine()
+
+            combine()
+            print(points)
+
+            if ask1 in starting_itm:
+                starting_itm.remove(ask1)
+
+            if ask2 in starting_itm:
+                starting_itm.remove(ask2)
+
+            starting_itm.append(random.choice(list(item)))
+            starting_itm.append(random.choice(list(item)))
+            if points >= starting_val:
+                play_again=input("play again y/n? ")
+                if play_again == "y":
+                    points = 0
+                    starting_itm = ()
+                    game()
+                else:
+                    print("bye")
+                    break
+
+game()
 
 
 
